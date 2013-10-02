f61c4d3b9293622e6740bf6e94b47606929621e0
ealonso <eudaldo.alonso@liferay.com>
2013-10-02 16:42:56 +0200

    Add *Indexer to the portlets

---
+++ sdk/portlets/jukebox-portlet/docroot/WEB-INF/src/org/liferay/jukebox/util/SongIndexer.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.liferay.jukebox.util;
+
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.BaseIndexer;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchEngineUtil;
+import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+
+import org.liferay.jukebox.model.Song;
+import org.liferay.jukebox.service.SongLocalServiceUtil;
+import org.liferay.jukebox.service.permission.SongPermission;
+import org.liferay.jukebox.service.persistence.SongActionableDynamicQuery;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class SongIndexer extends BaseIndexer {
+
+	public static final String[] CLASS_NAMES = {Song.class.getName()};
+
+	public static final String PORTLET_ID = PortletKeys.SONGS;
+
+	public SongIndexer() {
+		setPermissionAware(true);
+	}
+
+	@Override
+	public String[] getClassNames() {
+		return CLASS_NAMES;
+	}
+
+	@Override
+	public String getPortletId() {
+		return PORTLET_ID;
+	}
+
+	@Override
+	public boolean hasPermission(
+			PermissionChecker permissionChecker, String entryClassName,
+			long entryClassPK, String actionId)
+		throws Exception {
+
+		return SongPermission.contains(
+			permissionChecker, entryClassPK, ActionKeys.VIEW);
+	}
+
+	@Override
+	public void postProcessContextQuery(
+			BooleanQuery contextQuery, SearchContext searchContext)
+		throws Exception {
+
+		addStatus(contextQuery, searchContext);
+	}
+
+	@Override
+	protected void doDelete(Object obj) throws Exception {
+		Song song = (Song)obj;
+
+		deleteDocument(song.getCompanyId(), song.getSongId());
+	}
+
+	@Override
+	protected Document doGetDocument(Object obj) throws Exception {
+		Song song = (Song)obj;
+
+		Document document = getBaseModelDocument(PORTLET_ID, song);
+
+		document.addDate(Field.MODIFIED_DATE, song.getModifiedDate());
+		document.addText(Field.TITLE, song.getName());
+
+		return document;
+	}
+
+	@Override
+	protected Summary doGetSummary(
+		Document document, Locale locale, String snippet,
+		PortletURL portletURL) {
+
+		Summary summary = createSummary(document);
+
+		summary.setMaxContentLength(200);
+
+		return summary;
+	}
+
+	@Override
+	protected void doReindex(Object obj) throws Exception {
+		Song song = (Song)obj;
+
+		Document document = getDocument(song);
+
+		SearchEngineUtil.updateDocument(
+			getSearchEngineId(), song.getCompanyId(), document);
+	}
+
+	@Override
+	protected void doReindex(String className, long classPK) throws Exception {
+		Song song = SongLocalServiceUtil.getSong(classPK);
+
+		doReindex(song);
+	}
+
+	@Override
+	protected void doReindex(String[] ids) throws Exception {
+		long companyId = GetterUtil.getLong(ids[0]);
+
+		reindexEntries(companyId);
+	}
+
+	@Override
+	protected String getPortletId(SearchContext searchContext) {
+		return PORTLET_ID;
+	}
+
+	protected void reindexEntries(long companyId)
+		throws PortalException, SystemException {
+
+		final Collection<Document> documents = new ArrayList<Document>();
+
+		ActionableDynamicQuery actionableDynamicQuery =
+			new SongActionableDynamicQuery() {
+
+			@Override
+			protected void addCriteria(DynamicQuery dynamicQuery) {
+			}
+
+			@Override
+			protected void performAction(Object object) throws PortalException {
+				Song song = (Song)object;
+
+				Document document = getDocument(song);
+
+				documents.add(document);
+			}
+
+		};
+
+		actionableDynamicQuery.setCompanyId(companyId);
+
+		actionableDynamicQuery.performActions();
+
+		SearchEngineUtil.updateDocuments(
+			getSearchEngineId(), companyId, documents);
+	}
+
+}
