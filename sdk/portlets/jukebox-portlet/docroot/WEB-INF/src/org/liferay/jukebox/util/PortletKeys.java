473dbd6bd6aeb8c8c28023c6d4a9e40bf446a19a
ealonso <eudaldo.alonso@liferay.com>
2013-10-02 16:42:27 +0200

    Add PortletKeys

---
+++ sdk/portlets/jukebox-portlet/docroot/WEB-INF/src/org/liferay/jukebox/util/PortletKeys.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package org.liferay.jukebox.util;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class PortletKeys extends com.liferay.portal.util.PortletKeys {
+
+	public static final String ALBUMS = "albums_WAR_jukeboxportlet";
+
+	public static final String ARTISTS = "artists_WAR_jukeboxportlet";
+
+	public static final String SONGS = "songs_WAR_jukeboxportlet";
+
+}
