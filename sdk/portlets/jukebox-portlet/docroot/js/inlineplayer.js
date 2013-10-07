6a8620b45861b58a05e3e9dfa26e6d74e82356d5
Julio Camarero <julio.camarero@liferay.com>
2013-10-07 11:49:41 +0200

    Add flash player when html5 is not supported

--- sdk/portlets/jukebox-portlet/docroot/js/inlineplayer.js
+++ sdk/portlets/jukebox-portlet/docroot/js/inlineplayer.js
@@ -237,12 +237,12 @@ var inlinePlayer = null;
 
 soundManager.setup({
   // disable or enable debug output
-  debugMode: true,
+  debugMode: false,
   // use HTML5 audio for MP3/MP4, if available
   preferFlash: false,
   useFlashBlock: true,
   // path to directory containing SM2 SWF
-  url: '../../swf/',
+  url: 'swf/',
   // optional: enable MPEG-4/AAC support (requires flash 9)
   flashVersion: 9
 });
